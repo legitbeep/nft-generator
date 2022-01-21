b406c2e92b898ab763c3b4e690cd47b22b5e64e7
Harsh Yadav <glorytoharsh@gmail.com>
2022-01-21 12:30:01 +0000

    feat: completed tutorial

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ const editions = 1;
 const canvas = createCanvas(1000,1000);
 const ctx = canvas.getContext("2d");
 
-const saveLayer = (_canvas) => {
-    fs.writeFile("./output/newImg.png",_canvas.toBuffer("image/png")); // path, buffer
+const saveLayer = (_canvas,_edition) => {
+    fs.writeFile(`./output/${_edition}.png`,_canvas.toBuffer("image/png")); // path, buffer
 }
 
-const drawLayer = async(_layer, _editio) => {
+const drawLayer = async(_layer, _edition) => {
     let element = _layer[Math.floor(Math.random()+ _layer.elements.length)];
-    console.log(element);
-    // const img = loadImage("./input/img.png");
-    // ctx.drawImage(img,0,0,1000,1000); //(img,x,y,width,height);
-    // saveLayer(canvas);
+    if (element) {
+        const img = await loadImage(`${_layer.location}${element.fileName}`);
+        ctx.drawImage(
+            img,
+            _layer.position.x,
+            _layer.position.y,
+            _layer.size.width,
+            _layer.size.height
+        ); //(img,x,y,width,height);
+        console.log(`Added ${_layer.name} and chose ${element.name}`);
+        saveLayer(canvas,_edition);
+    }
 }
 
 for(let i=1; i<=editions; i++){
