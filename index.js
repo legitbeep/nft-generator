ce2698ac86390b25b6e8e85b021c636829098dcf
Harsh Yadav <glorytoharsh@gmail.com>
2022-01-21 12:12:59 +0000

    feat: add folders for input

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const { createCanvas, loadImage } = require("canvas");
 const { layers, width, height } = require("./input/config.js");
+const editions = 1;
 
 const canvas = createCanvas(1000,1000);
 const ctx = canvas.getContext("2d");
@@ -10,10 +11,16 @@ const saveLayer = (_canvas) => {
     fs.writeFile("./output/newImg.png",_canvas.toBuffer("image/png")); // path, buffer
 }
 
-const drawLayer = async() => {
-    const image = loadImage("./input/img.png");
-    ctx.drawImage(img,0,0,1000,1000); //(img,x,y,width,height);
-    saveLayer(canvas);
+const drawLayer = async(_layer, _editio) => {
+    let element = _layer[Math.floor(Math.random()+ _layer.elements.length)];
+    console.log(element);
+    // const img = loadImage("./input/img.png");
+    // ctx.drawImage(img,0,0,1000,1000); //(img,x,y,width,height);
+    // saveLayer(canvas);
 }
 
-drawLayer();
+for(let i=1; i<=editions; i++){
+    layers.forEach((layer) => {
+        drawLayer(layer,i);
+    })
+}
