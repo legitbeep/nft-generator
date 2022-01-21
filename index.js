edabe675e666c5c8d525a8e38223460520c2c9f6
Harsh Yadav <glorytoharsh@gmail.com>
2022-01-21 20:00:38 +0000

    feat: add assets

--- index.js
+++ index.js
@@ -2,18 +2,35 @@
 const fs = require("fs");
 const { createCanvas, loadImage } = require("canvas");
 const { layers, width, height } = require("./input/config.js");
-const editions = 1;
 
-const canvas = createCanvas(1000,1000);
+const canvas = createCanvas(width,height);
 const ctx = canvas.getContext("2d");
 
-const saveLayer = (_canvas,_edition) => {
-    fs.writeFile(`./output/${_edition}.png`,_canvas.toBuffer("image/png")); // path, buffer
+const getEdition = () => {
+    let totalCombinations = 0;
+    for(let i = 1; i<layers.length; i++){
+        totalCombinations *= layers[i].elements.length;
+    }
+    return totalCombinations || 1;
 }
 
-const drawLayer = async(_layer, _edition) => {
-    let element = _layer[Math.floor(Math.random()+ _layer.elements.length)];
-    if (element) {
+// to store combinations generated
+const generated = [];
+const editions = getEdition();
+
+const saveLayer = (_canvas,_edition) => {
+    fs.writeFileSync(`./output/${_edition}.png`,_canvas.toBuffer("image/png")); // path, buffer
+    console.log("added image"+_edition);
+}
+
+const drawLayer = async(_layer, _edition, index) => {
+    let element ;
+    //if ( _layer.id == 1 ){
+        element = _layer.elements[Math.floor(Math.random()*_layer.elements.length)];
+    // } else {
+    //     element = _layer.elements[index];
+    // }
+    if (element && element.fileName) {
         const img = await loadImage(`${_layer.location}${element.fileName}`);
         ctx.drawImage(
             img,
@@ -22,13 +39,32 @@ const drawLayer = async(_layer, _edition) => {
             _layer.size.width,
             _layer.size.height
         ); //(img,x,y,width,height);
-        console.log(`Added ${_layer.name} and chose ${element.name}`);
+        //console.log(`Added ${_layer.name} and chose ${element.name}`);
         saveLayer(canvas,_edition);
     }
 }
 
-for(let i=1; i<=editions; i++){
-    layers.forEach((layer) => {
-        drawLayer(layer,i);
-    })
+const findUniqueCombination = () => {
+    let indices = [1,1,1,1];
+    let curCombination = "1111";
+    while(generated.includes(curCombination)){
+        curCombination = "";
+        indices = [...layers.map(_layer => (_layer.id !== 1 && Math.floor(Math.random()*_layer.elements.length)))];
+        indices.forEach(idx => {
+            curCombination += idx+"";
+        })
+    }
+    generated.push(curCombination);
+    return indices;
+}
+
+for(let i=1; i<=10; i++){
+    let indices = findUniqueCombination();
+    try {
+        layers.forEach((layer,idx) => {
+            drawLayer(layer,i,indices[idx]);
+        })
+    } catch(err) {
+        console.log(err);
+    }
 }
