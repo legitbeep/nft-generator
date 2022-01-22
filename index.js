409086f19dac32ad12ea353c5d387b83fd9201d4
Harsh Yadav <glorytoharsh@gmail.com>
2022-01-22 09:42:01 +0000

    feat: complete my changes

--- index.js
+++ index.js
@@ -6,30 +6,43 @@ const { layers, width, height } = require("./input/config.js");
 const canvas = createCanvas(width,height);
 const ctx = canvas.getContext("2d");
 
+const fNames = ["Galctus", "Filthy", "Big", "Deep", "Yeet", "Yeeger", "Yoink","Sansa", "Arther", "Skye", "Fart", "Master","Crime Master", "Maze", "Goblin", "Shady", "Kanye", "The", "No", "Sky","Ultra", "Wierd", "Strong", "Weak", "Broad", "Heavy", ..."trashy tubular nasty jacked swol buff ferocious firey flamin agnostic anxious artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical".split(' ')];
+const lNames = ["king", "chung", "bro", "bart", "eren", "yeeter","man", "woman", "luke", "walker", "frank", "gogo","slim", "pac", "south", "wolf", "bread", "flower","thing", "cap","legend", "fired", "alien", "jaadu", "gangsta", "jedi", "fox", "silver", "pasta", "chief", "barbarian", "myth", "furry", "barter", "agent", "theif", "potato"]; 
+
+const takenNames =[""];
+const generated = [];
+
 const getEdition = () => {
-    let totalCombinations = 0;
+    let totalCombinations = 1;
     for(let i = 1; i<layers.length; i++){
         totalCombinations *= layers[i].elements.length;
     }
-    return totalCombinations || 1;
+    return totalCombinations ;
 }
 
-// to store combinations generated
-const generated = [];
 const editions = getEdition();
 
-const saveLayer = (_canvas,_edition) => {
-    fs.writeFileSync(`./output/${_edition}.png`,_canvas.toBuffer("image/png")); // path, buffer
-    console.log("added image"+_edition);
+const saveLayer = (_canvas,_name) => {
+    fs.writeFileSync(`./output/${_name}.png`,_canvas.toBuffer("image/png")); // path, buffer
+    console.log("added image"+_name);
 }
 
-const drawLayer = async(_layer, _edition, index) => {
-    let element ;
-    //if ( _layer.id == 1 ){
-        element = _layer.elements[Math.floor(Math.random()*_layer.elements.length)];
-    // } else {
-    //     element = _layer.elements[index];
-    // }
+const saveMeta = (name) => {
+    const meta = {
+        name,
+        description: `A drawing of ${name}`,
+        image: `${name}.png`,
+        attributes: [
+            { 
+                rarity: 0.5
+            }
+        ]
+    }
+    writeFileSync(`./output/${name}.json`, JSON.stringify(meta))
+}
+
+const drawLayer = async(_layer, _name, index) => {
+    let element = _layer.elements[index];
     if (element && element.fileName) {
         const img = await loadImage(`${_layer.location}${element.fileName}`);
         ctx.drawImage(
@@ -40,31 +53,49 @@ const drawLayer = async(_layer, _edition, index) => {
             _layer.size.height
         ); //(img,x,y,width,height);
         //console.log(`Added ${_layer.name} and chose ${element.name}`);
-        saveLayer(canvas,_edition);
+        saveLayer(canvas,_name);
+        saveMeta(_name);
     }
 }
 
-const findUniqueCombination = () => {
-    let indices = [1,1,1,1];
-    let curCombination = "1111";
-    while(generated.includes(curCombination)){
+const getFirstCombination = () => {
+    let _curCombination = "", _finalCom = [];
+    layers.forEach((_layer) => {
+        if (_layer.id !== 1) {
+            _curCombination = _curCombination + "0" ;
+            _finalCom.push(_layer.elements.length-1);
+        }
+    })
+    return [_curCombination, _finalCom];
+}
+
+let [curCombination, finalCombination] = getFirstCombination();
+console.log(fNames.length*lNames.length,fNames,curCombination,finalCombination);
+let indices = [];
+for(let i = 0 ; i<curCombination.length; i++){
+    indices.push(parseInt(curCombination[i]));
+}
+let curName = "";
+
+while(generated.length < editions || curCombination == finalCombination){
+    while(generated.includes(curCombination || curCombination == finalCombination )){
+        for(let i = indices.length; i>0; i--){
+            if( indices[i] == finalCombination[i] ){
+                for(let j = i ; j<indices.length; j++){
+                    indices[j] = 0;
+                }
+                indices[i-1]++;
+            }
+        }
         curCombination = "";
-        indices = [...layers.map(_layer => (_layer.id !== 1 && Math.floor(Math.random()*_layer.elements.length)))];
-        indices.forEach(idx => {
-            curCombination += idx+"";
-        })
+        indices.forEach(idx => curCombination += idx + "");
+    }
+    while(takenNames.includes(curName)){
+        curName = fNames[Math.floor(Math.random()*fNames.length)] + " " + lNames[Math.floor(Math.random()*lNames.length)]
     }
     generated.push(curCombination);
-    return indices;
-}
-
-for(let i=1; i<=10; i++){
-    let indices = findUniqueCombination();
-    try {
-        layers.forEach((layer,idx) => {
-            drawLayer(layer,i,indices[idx]);
-        })
-    } catch(err) {
-        console.log(err);
-    }
+    takenNames.push(curName);
+    layers.forEach((lyr,idx) => {
+        drawLayer(lyr, curName, lyr.id == 1 ? Math.floor(Math.random()*lyr.elements.length) : indices[idx-1])
+    })
 }
